92600d4cb9bf84d772bf1baad11997f5e554a2df
Simran Mandal <simranmandal0211@gmail.com>
2023-06-18 01:32:23 +0530

    Initialize variable

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('Working');
+
+let tasks = [];
+const taskList = document.getElementById('list');
+const addTaskInput = document.getElementById('add');
+const tasksCounter = document.getElementById('tasks-counter');
+
