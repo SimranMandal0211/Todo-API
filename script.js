1a871d2ce58b01da1b4b0aadeacb1eb73986008b
Simran Mandal <simranmandal0211@gmail.com>
2023-06-19 00:45:01 +0530

    Add function to do task

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
 
+function addTaskToDOM(task){
+
+}
+
+function renderList(){
+
+}
+
+function toggleTask(taskId){
+
+}
+
+function deleteTask(taskId){
+
+}
+
+function addTask(task){
+
+}
+
+function showNotification(text){
+
+}
+
+function handelInputKeypress(e){
+
+}
+
+function handleClickListener(e){
+
+}
